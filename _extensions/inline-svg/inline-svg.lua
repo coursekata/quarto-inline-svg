2db21c66db75b1be278ee48b3e3134c115f63340
Adam Blake <adamblake@g.ucla.edu>
2023-03-21 10:47:23 -0700

    feat!: inline as base64 data URI instead of raw SVG string
    The advantage is that we don't have to do any extra processing for other Quarto features. Whatever
    Quarto supports for figures and images in HTML and markdown, this will be compatible with.

--- _extensions/inline-svg/inline-svg.lua
+++ _extensions/inline-svg/inline-svg.lua
@@ -1,3 +1,35 @@
+---Base64 encoder.
+---@param data string
+---@return string
+---Copyright: Lua 5.1+ base64 v3.0 (c) 2009 by Alex Kloss <alexthkloss@web.de>.
+---License: LGPL2.
+---@source http://lua-users.org/wiki/BaseSixtyFour
+function base64(data)
+  local chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+  return ((data:gsub('.', function(x)
+        local r, b = '', x:byte()
+        for i = 8, 1, -1 do r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and '1' or '0') end
+        return r;
+      end) .. '0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c = 0
+        for i = 1, 6 do c = c + (x:sub(i, i) == '1' and 2 ^ (6 - i) or 0) end
+        return chars:sub(c + 1, c + 1)
+      end) .. ({ '', '==', '=' })[#data % 3 + 1])
+end
+
+---Split a string by character sequence.
+---@param str string a string that may have multiple lines in it
+---@param chars string a character string to split on
+---@return table parts a table with one entry per part in the string (squished)
+local function split(str, chars)
+  parts = {}
+  for line in str:gmatch("[^" .. chars .. "]+") do
+    table.insert(parts, line)
+  end
+  return parts
+end
+
 ---Determine if a file exists and is readable.
 ---@param path string the file to check
 ---@return boolean is_readable whether the file exists and is readable
@@ -19,52 +51,14 @@ local function read(path)
   return contents
 end
 
----Get the basename from a file path.
----@param path string the file path
----@return string
-local function basename(path)
-  return path:match('[\\/]([^\\/]+)$')
-end
-
----Remove the suffix from a file path.
----@param path string the file path to modify
----@return string name_only the name of the file or directory with no extension
-local function remove_extension(path)
-  local removed = path:match("(.+)%..+$")
-  if (removed == nil) then
-    return path
-  end
-  return removed
-end
-
----Check if string ends with substring.
----@param str string the string to check
----@param ending string the substring to search end of string for
----@return boolean matched whether the string ends with the substring
----@source http://lua-users.org/wiki/StringRecipes
-local function ends_with(str, ending)
-  return ending == "" or str:sub(- #ending) == ending
-end
-
-
----Use a default value if the given string is empty.
----@param str string the string to check
----@param default string the default to use if the string is empty
----@return string result either the string or the default
-local function default_string(str, default)
-  return (str and str ~= '') and str or default
-end
-
----Split a string by character sequence.
----@param str string a string that may have multiple lines in it
----@param chars string a character string to split on
----@return table parts a table with one entry per part in the string (squished)
-local function split(str, chars)
-  parts = {}
-  for line in str:gmatch("[^" .. chars .. "]+") do
-    table.insert(parts, line)
-  end
-  return parts
+---Read an SVG file to string, discarding the XML header tag.
+---@param path string the path to the SVG file
+---@return string contents the contents of the file (less the <xml> header tag)
+local function read_svg(path)
+  local svg_content = read(path)
+  local svg_lines = split(svg_content, "\r\n")
+  table.remove(svg_lines, 1)
+  return table.concat(svg_lines, "")
 end
 
 ---Match any of the given strings.
@@ -80,29 +74,22 @@ local function match_any(str, targets)
   return false
 end
 
+---Check if string ends with substring.
+---@param str string the string to check
+---@param ending string the substring to search end of string for
+---@return boolean matched whether the string ends with the substring
+---@source http://lua-users.org/wiki/StringRecipes
+local function ends_with(str, ending)
+  return ending == "" or str:sub(- #ending) == ending
+end
+
 -- Find references to generated SVGs and replace with SVGs.
 local formats = { "html*", "markdown*", "commonmark*", "gfm", "markua" }
 if match_any(FORMAT, formats) then
   function Image(elem)
-    local title = default_string(elem.attr['title'], "Untitled figure")
-    local desc = default_string(elem.attr['alt'], "No description")
     if file_exists(elem.src) and ends_with(elem.src, ".svg") then
-      -- image name is unique, so we can use it for IDs
-      local img_name = remove_extension(basename(elem.src))
-      local title_id = img_name .. "--title"
-      local desc_id = img_name .. "--desc"
-
-      -- read the SVG and add the accessibility components
-      local svg_content = read(elem.src)
-      svg_lines = split(svg_content, "\r\n")
-      table.remove(svg_lines, 1)
-      svg_lines[1] = svg_lines[1]:gsub(">$",
-        string.format(' role="img" aria-labelledby="%s" aria-describedby="%s">', title_id, desc_id))
-      table.insert(svg_lines, 2, string.format('<title id="%s">%s</title>', title_id, title))
-      table.insert(svg_lines, 3, string.format('<desc id="%s">%s</desc>', desc_id, desc))
-
-      -- inline the SVG
-      elem = pandoc.RawInline("html", table.concat(svg_lines, ""))
+      local svg = read_svg(elem.src)
+      elem.src = "data:image/svg+xml;base64," .. base64(svg)
       return { elem }
     end
   end
